ffa0c7494d64f094a379a154b40b75dc3744975d
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-08-12 17:15:44 +0530

    added Linear search ☺😁

--- src/Algorithms/Search.js
+++ src/Algorithms/Search.js
@@ -1,7 +1,29 @@
+// function to implement binary search
 exports.BinarySearch=()=>{
     console.log("This is Binary Search")
 }
 
-exports.LinearSearch = ()=>{
-    console.log("this is linear search")
+
+// function to implement linear search 
+
+exports.LinearSearch = (key,list)=>{
+    // Time Complexity O(N)
+    let present = false;
+    for(let i = 0 ; i<list.length ; i++){
+        if(list[i]===key)
+            return true;
+    }
+    return present;
 }
+
+
+// function to implement lower bound
+exports.LowerBound=(ley,list)=>{
+
+}
+
+// function to implement upper bound
+exports.UpperBound=(key,list)=>{
+
+}
+
