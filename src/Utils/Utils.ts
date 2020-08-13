e1806e72091d5e12eb76a944ce9dddc8602f1b17
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-08-13 21:32:03 +0530

    added function signatures

---
+++ src/Utils/Utils.ts
@@ -0,0 +1,93 @@
+/*
+Swap function , Min , Max function , reverse , reverse_till , accumulate , count , next_permutation , distance , even , odd , isMultiple , isPower 
+isAlphaNumeric , isVowel , isConsonant , isPrime ,  
+*/
+
+// Implementation of swap function , that interchanges the value 
+const swap=(first : any , second :any)=>{
+
+}
+
+// function to calculate minimum of two values
+const min=(first:Number,second:Number)=>{
+
+}
+
+//function to calculate maximum of two values
+const max=(first:Number,second:Number)=>{
+
+}
+
+// function to reverse an array
+const reverse=(item:any)=>{
+
+}
+
+// function to process reversing upto certain end points
+const reverse_till=(item:any)=>{
+
+}
+
+// function to process accumulation of items inside a list
+const accumulate=(item:Array<Number>,initial_value : Number = 0)=>{
+
+}
+
+// function to count occurences 
+const count=(value:Number,ls:any)=>{
+
+}
+
+// function to find distance
+const distance=(item:any)=>{
+
+}
+
+/*  Common mathematical utility functions */
+
+// function to check even
+const isEven=(number : Number)=>{
+
+}
+
+//function to check odd
+const isOdd=(number: Number)=>{
+
+}
+
+// function to check isMultiple of
+const isMultipleOf=(first : Number,second:Number)=>{
+
+}
+
+// function to check is power of
+const isPowerOf=(first:Number ,second:Number)=>{
+
+}
+
+// function to check perfect square
+const isPerfectSquare=(number:Number)=>{
+
+}
+
+// function to check is prime
+const isPrime=(number:Number)=>{
+
+}
+
+/* common character based utility functions */
+
+//function to check is alphanumeric
+const isAlphaNumeric=(item : any)=>{
+
+}
+
+// function to check is vowel
+const isVowel=(item:String)=>{
+
+}
+
+// function to check is consonant
+const isConsonant=(item:String)=>{
+    
+}
