e1808c4336232c872f8bd175c0e95cccd5b27168
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-08-14 17:02:18 +0530

    added Triplet Class and Tests

---
+++ src/ds/Triplet.ts
@@ -0,0 +1,42 @@
+/* Implementtaion of Triplet class  */
+
+/**
+ * Get All elments , first second third
+ * to List
+ * to string
+ */
+
+export class Triplet<F, S, T> {
+  private _first: F;
+  private _second: S;
+  private _third: T;
+
+  constructor(first: F, second: S, third: T) {
+    this._first = first;
+    this._second = second;
+    this._third = third;
+  }
+
+  // get the first element
+  get first(): F {
+    return this._first;
+  }
+
+  // get the second element
+  get second(): S {
+    return this._second;
+  }
+
+  get third(): T {
+    return this._third;
+  }
+
+  toString() {
+    return (this._first + " " + this._second + " " + this._third).toString();
+  }
+
+  toList() {
+    let ls = [this._first, this._second, this._third];
+    return ls;
+  }
+}
