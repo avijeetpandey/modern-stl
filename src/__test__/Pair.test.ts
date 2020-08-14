f84b39f84c7b8aac015a58ec182b2cc740f541a4
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-08-14 16:53:12 +0530

    added new P test

--- src/__test__/Pair.test.ts
+++ src/__test__/Pair.test.ts
@@ -2,7 +2,7 @@ import {Pair} from '../ds/Pair'
 
 describe('Pair tests', () => {
     test('Init', () => {
-        const pair = new Pair('Hi mom!', 42);
+        const pair = new Pair<String,number>('Hi mom!', 42);
 
         expect(pair.first).toBe('Hi mom!');
         expect(pair.second).toBe(42);
