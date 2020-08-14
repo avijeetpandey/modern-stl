b21e807188e5f22050245761985bbf43994d1219
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-08-14 17:27:42 +0530

    refactored tests

---
+++ __test__/Pair.test.ts
@@ -0,0 +1,11 @@
+import {Pair} from './../src/ds/Pair'
+
+describe("Pair tests", () => {
+  test("Init", () => {
+    const pair = new Pair<String, number>("Hi mom!", 42);
+
+    expect(pair.first).toBe("Hi mom!");
+    expect(pair.second).toBe(42);
+    expect(pair.toString()).toBe("Hi mom! 42");
+  });
+});
---
+++ __test__/Triplet.test.ts
@@ -0,0 +1,13 @@
+import { Triplet } from "../src/ds/Triplet"
+
+describe("Triplet Tests ", () => {
+  test("Init", () => {
+    const triplet = new Triplet<number, string, string>(1, "Avijeet", "B+");
+
+    expect(triplet.first).toBe(1);
+    expect(triplet.second).toBe("Avijeet");
+    expect(triplet.third).toBe("B+");
+    expect(triplet.toList()).toStrictEqual([1, "Avijeet", "B+"]);
+    expect(triplet.toString()).toBe("1 Avijeet B+");
+  });
+});
