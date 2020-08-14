d14ea2cfd46b12cd3ba2b70162268818834dfae0
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-08-14 15:55:47 +0530

    added Pair class Test

---
+++ src/__test__/Pair.test.ts
@@ -0,0 +1,10 @@
+import {Pair} from '../ds/Pair'
+
+describe('Pair tests', () => {
+    test('Init', () => {
+        const pair = new Pair('Hi mom!', 42);
+
+        expect(pair.first).toBe('Hi mom!');
+        expect(pair.second).toBe(42);
+    });
+})
