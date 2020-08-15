a70448ecec17f79e27de62fe0cbb3c7a977c0b5f
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-08-15 14:36:47 +0530

    added LinkedList class

--- src/ds/LinkedList.ts
+++ src/ds/LinkedList.ts
@@ -1,34 +1,140 @@
-// /** 
-//  * Implementation of LinkedList Class
-//  * Operations -> insert , append , delete , find , size , isEmpty, appendToEnd
-//  */
+/** 
+ * Implementation of LinkedList Class
+ * Operations -> insert , append , delete , find , size , isEmpty, appendToEnd
+ */
 
 
-//  /**
-//   * Definintion of node class 
-//   */
+ /**
+  * Definintion of node class 
+  */
  
-//  class Node<T>{
-//     public next:Node<T> | null = null;
-//     public prev:Node<T> | null = null;
-//     constructor(public data:T){}
-// }
+ export class Node<T>{
+    public next:Node<T> | null = null;
+    public prev:Node<T> | null = null;
+    constructor(public data:T){}
+}
 
-// interface ILinkedList<T>{
-//     insertAtBegin(data:T) : Node<T>
-//     insertAtEnd(data:T) : Node<T>
-//     deleteNode(data:T) : Node<T>
-//     find(data:T) : boolean
-//     findIndex(data:T) : number
-//     length() : number
-//     printList(): void
-//     toString(): string
-//     toList() : Array<T>
-// }
+interface ILinkedList<T>{
+    insertAtBegin(data:T) : Node<T>
+    insertAtEnd(data:T) : Node<T>
+    deleteNode(data:Node<T>) : void
+    find(data:T) : boolean
+    findIndex(data:T) : number
+    size() : number
+    list(): T[]
+    toString(): string
+}
 
-// /**
-//  * Definition of the LinkedList Class 
-//  */
-// // export class LinkedList<T> implements ILinkedList<T>{
+/**
+ * Definition of the LinkedList Class 
+ */
+export class LinkedList<T> implements ILinkedList<T>{
+    // head of the linkedList
+    private head : Node<T> | null = null
 
-// // }
+    /**
+     * 
+     * @param {data} value to be inserted
+     * @returns {node} inserted Node
+     */
+    public insertAtBegin(data:T) : Node<T> {
+        const node  = new Node(data)
+        if(!this.head){
+            this.head=new Node(data)
+        }else{
+            this.head.prev = node
+            node.next = this.head
+            this.head = node
+        }
+        return node
+    }
+
+    /**
+     * 
+     * @param {data} -> Tha value that needs to be inserted 
+     * @returns {node } the last inserted node
+     */
+
+    public insertAtEnd(data:T) : Node<T>{
+        const node = new Node(data)
+        if(!this.head){
+            this.head = node
+        }else{
+            const getLast = (node : Node<T>) : Node<T> =>{
+                return node.next ? getLast(node.next) : node
+            }
+
+            const lastNode = getLast(this.head)
+            node.prev = lastNode
+            lastNode.next = node
+        }
+        return node
+    }
+
+    /**
+     * @params {data} -> data to be deleted
+     * @returns {void}
+     */
+    public deleteNode(node:Node<T>) : void{
+        if(!node.prev){
+            this.head = node.next
+        }else{
+            const prevNode = node.prev
+            prevNode.next = node.next
+        }
+    }
+
+    /**
+     * @returns {T[]} list of Nodes in the linked list
+     */
+    public list() : T[]{
+        const linkedList : T[] = []
+        if(!this.head){
+            return linkedList
+        }
+        const add=(node:Node<T>) : T[] =>{
+            linkedList.push(node.data)
+            return node.next ? add(node.next) : linkedList
+        }
+        return linkedList
+    }
+
+    /**
+     * @returns {length} length of the linked list
+     */
+    public size():number{
+        return this.list().length
+    }
+
+    /**
+     * 
+     * @param data data to be searched 
+     * @returns {boolean} true or false
+     */ 
+    public find(data:T) : boolean {
+        let list : T[] = this.list()
+        for(let i of list)
+            if(i === data)
+                return true
+        return false
+    }
+    /**
+     * 
+     * @param {data} element to be searched
+     * @returns {index} index of the element 
+     */ 
+    public findIndex(data:T) : number{
+        let linkedList : T[] = this.list()
+        for(let i=0;i<linkedList.length;i++){
+            if(linkedList[i]===data)
+                return i
+        }
+    return -1
+    }
+    /**
+     * @returns {list to elements as string}
+     */
+    public toString():string {
+        return this.list().toString()
+    }
+}
