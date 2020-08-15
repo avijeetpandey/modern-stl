b23b2791a05fe8b2aef65e37fc58f2a79462c482
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-08-15 21:35:21 +0530

    added fast_exponentiation

--- src/algoritms/Maths.ts
+++ src/algoritms/Maths.ts
@@ -1,3 +1,13 @@
 /*
 GCD , LCM , factors  , add , subtract , multiply , fast exponentiation , generate fibonacci numbers , seive and segmented sieve
 */
+export const fast_exponentiation = (a: number, b: number): number => {
+  if (b === 0) return 1;
+  // Breaking the problem into furthur subproblems
+  let subproblem: number = fast_exponentiation(a, b / 2);
+
+  if (b & 1) {
+    return a * subproblem * subproblem;
+  }
+  return subproblem * subproblem;
+};
