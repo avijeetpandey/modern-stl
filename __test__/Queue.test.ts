90ffcc5a181a5f074c887b84514a883d49d0feea
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-08-17 13:24:08 +0530

    formated documents

--- __test__/Queue.test.ts
+++ __test__/Queue.test.ts
@@ -1,18 +1,17 @@
-import {Queue} from '../src/ds/Queue'
+import { Queue } from "../src/ds/Queue";
 
-describe('Queue Data Structure Test',()=>{
-    test('Queue Init Test',()=>{
-        let q = new Queue()
-        q.push(45)
-        q.push(89)
-        expect(q.front()).toBe(45)
-        q.remove(45)
-        expect(q.front()).toBe(89)
-        expect(q.size()).toBe(1)
-        expect(q.isEmpty()).toBe(false)
-        for(let i=0;i<10000 ;i++)
-            q.push(i)
-        expect(q.front()).toBe(89)
-        expect(q.size()).toBe(10001)
-    })
-})
+describe("Queue Data Structure Test", () => {
+  test("Queue Init Test", () => {
+    let q = new Queue();
+    q.push(45);
+    q.push(89);
+    expect(q.front()).toBe(45);
+    q.remove(45);
+    expect(q.front()).toBe(89);
+    expect(q.size()).toBe(1);
+    expect(q.isEmpty()).toBe(false);
+    for (let i = 0; i < 10000; i++) q.push(i);
+    expect(q.front()).toBe(89);
+    expect(q.size()).toBe(10001);
+  });
+});
--- src/ds/Queue.ts
+++ src/ds/Queue.ts
@@ -1,45 +1,60 @@
 /**
  * @author {Avijeet Pandey}
  */
-import {LinkedList} from './LinkedList'
+import { LinkedList } from "./LinkedList";
 
-export interface IQueue<T>{
-    push(data : T) : void
-    remove(data:T) : boolean
-    size(data : T) : number
-    isEmpty()  : boolean
-    front() : T
+export interface IQueue<T> {
+  push(data: T): void;
+  remove(data: T): boolean;
+  size(data: T): number;
+  isEmpty(): boolean;
+  front(): T;
 }
-export class Queue<T> implements IQueue<T>{
-    private q : LinkedList<T>
-    constructor(){
-        this.q = new LinkedList<T>()
-    }
+export class Queue<T> implements IQueue<T> {
+  private q: LinkedList<T>;
+  constructor() {
+    this.q = new LinkedList<T>();
+  }
 
-    /**
-     * Basic Queue Operations
-     */
-    public front() : T{
-        return this.q.getHead()
-    }
+  /**
+   * Basic Queue Operations
+   */
+  /**
+   * @returns {front} front of the queue
+   */
+  public front(): T {
+    return this.q.getHead();
+  }
+  /**
+   * @param {data} inserts data into the queue
+   */
+  public push(data: T) {
+    this.q.insertAtEnd(data);
+  }
+  /**
+   * @returns {boolean} removes from the queue
+   */
 
-    public push(data : T){
-        this.q.insertAtEnd(data)
-    }
+  public remove(data: T): boolean {
+    return this.q.deleteNode(data);
+  }
 
-    public remove(data : T){
-        return this.q.deleteNode(data)
-    }
-
-    public size() : number{
-        return this.q.size()
-    }
-
-    public toList() : T[]{
-        return this.q.list()
-    }
-
-    public isEmpty() : boolean{
-        return this.q.size() > 0 ? false : true
-    }
+  /**
+   * @returns {size}  of the queue
+   */
+  public size(): number {
+    return this.q.size();
+  }
+  /**
+   * @returns {List}  of the queue
+   */
+  public toList(): T[] {
+    return this.q.list();
+  }
+  /**
+   * @returns {boolean} weather queue is empty or not
+   */
+  public isEmpty(): boolean {
+    return this.q.size() > 0 ? false : true;
+  }
 }
